3995a0978f55fc1022b0345821b42f5ff1103b2a
anurag <23anuragsharma@gmail.com>
2023-11-16 17:07:35 +0530

    Search

--- REACT-FILTER/src/App.jsx
+++ REACT-FILTER/src/App.jsx
@@ -2,18 +2,29 @@ import { useState } from "react";
 import { Items } from "./items";
 import "./App.css";
 
-
 const App = () => {
   const [items, setItems] = useState(Items);
+  const [search, setSearch] = useState("");
 
   const filterItems = (category) => {
+   
     if (category === "All") {
       setItems(Items);
     } else {
       const filteredItems = Items.filter((item) => item.category === category);
       setItems(filteredItems);
     }
+
+    if (search !== "") {
+      const filteredBySearch = Items.filter((item) =>
+        item.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setItems(filteredBySearch);
+    }
+
   };
+
+
 
   return (
     <div className="container mx-auto">
@@ -23,7 +34,6 @@ const App = () => {
         </h1>
       </div>
       <div className="buttons mt-10 flex justify-center space-x-4">
-      
         <button
           className="btn text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
           onClick={() => filterItems("All")}
@@ -54,7 +64,39 @@ const App = () => {
         >
           Jewelry
         </button>
+        <div className="flex items-center justify-center ">
+          <input
+            type="text"
+            placeholder="Search here"
+            className="bg-gray-700 rounded-lg h-11 text-ms font-medium text-white"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            type="button"
+            className=" ml-2 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            onClick={() => filterItems("All")}
+         
+         >
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+            <span className="sr-only">Icon description</span>
+          </button>
+        </div>
       </div>
+
       <div className="products flex flex-wrap justify-center mt-10">
         {items.map((item) => (
           <div
@@ -68,7 +110,6 @@ const App = () => {
             />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{item.title}</div>
-         
             </div>
             <div className="px-6 pt-4 pb-2">
               <p className="text-gray-700 text-base">Price: ${item.price}</p>
