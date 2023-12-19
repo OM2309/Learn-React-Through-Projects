1a87e2430c47f6c0b3d847ad80d5adfc7490265f
anurag <23anuragsharma@gmail.com>
2023-12-19 22:30:30 +0530

    Redux toolkit

--- Todo-Mern/server/server.js
+++ Todo-Mern/server/server.js
@@ -3,12 +3,13 @@ import express from "express";
 import connectDB from "./config/db.js";
 import todoRouter from "./routes/todoRoutes.js";
 import bodyParser from "body-parser";
-
+import cors from 'cors'
 // Constants
 const app = express();
 const PORT = 4444;
 
 // Middlewaren and Config handlers
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
